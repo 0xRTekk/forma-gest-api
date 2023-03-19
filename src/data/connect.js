a7ff02936de5df914068d259eea1bcd93c6f68d7
RemOclock <remi.sulpice@oclock.io>
2023-03-19 18:11:46 +0100

    Sequelize connect

--- src/data/connect.js
+++ src/data/connect.js
@@ -4,8 +4,9 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.PG_URL, {
   define: {
     timestamps: true, // Adds createdAt and updatedAt timestamps to the model. Default true.
-    createdAt: "created_at", // Override the name of the createdAt column if a string is provided, or disable it if false.
-    updatedAt: "updated_at", // Override the name of the updatedAt column if a string is provided, or disable it if false.
+    underscored: true,
+    // createdAt: "created_at", // Override the name of the createdAt column if a string is provided, or disable it if false.
+    // updatedAt: "updated_at", // Override the name of the updatedAt column if a string is provided, or disable it if false.
   },
   // logging: false, // Pour empêcher de logger les requêtes SQL faites à la BDD en console Node
   // query: { raw: true } // Permet de renvoyer les données brutes de la BDD sans les wrapper dans les models sequelize qui sont difficile à lire en console.
